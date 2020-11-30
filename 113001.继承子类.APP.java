22c17a0c7d1f531b67d9d21e586f34e32a056c96
meng139799 <74774248+meng139799@users.noreply.github.com>
2020-11-30 15:38:35 +0800

    Create 113001.继承子类.APP.java

---
+++ 113001.继承子类.APP.java
@@ -0,0 +1,13 @@
+package pac;
+
+public class APP extends R{
+	public static void main(String[] args) {
+		System.out.println(R.boy_filePath);
+		System.out.println(R.girl_filePath);
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				System.out.print(array2d[i][j]);
+			}
+		}
+	}
+}
