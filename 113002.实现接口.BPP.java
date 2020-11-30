048375651c50c8e40b731e75a6115064d22d1ae9
meng139799 <74774248+meng139799@users.noreply.github.com>
2020-11-30 15:40:37 +0800

    Create 113002.实现接口.BPP.java

---
+++ 113002.实现接口.BPP.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class BPP implements S {
+	public static void main(String[] args) {
+		System.out.println(S.boy_filePath);
+		System.out.println(S.girl_filePath);
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				System.out.print(array2d[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
